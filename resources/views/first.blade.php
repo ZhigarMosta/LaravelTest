976378556efb5baf95081f0bcbdf2ac74cae0776
ZhigarMosta <ZhigarKirill@gmail.com>
2024-09-19 13:01:05 +0500

    create_project

---
+++ resources/views/first.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/style/style.css">
+</head>
+<body>
+    <h1>Первая страница</h1>
+    <p>{{$sum}} {{$a}}</p>
+    <p>текст new</p>
+    <p>{{$a}}</p>
+</body>
+</html>
