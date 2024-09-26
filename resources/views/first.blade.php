a482dab708c50031cfa82b281d7a39590209462a
ZhigarMosta <ZhigarKirill@gmail.com>
2024-09-26 12:03:45 +0500

    шаблонизатор

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/style/style.css">
-</head>
-<body>
+@extends('layout.main')
+@section('content')
     <h1>Первая страница</h1>
     <p>{{$sum}} {{$a}}</p>
     <p>текст new</p>
     <p>{{$a}}</p>
-</body>
-</html>
+@endsection()
